46bcba94bee21490d21ddf18f31a3e9a1ddee8b2
AnttiSEPP <antti.seppanenn@gmail.com>
2023-10-13 13:58:45 +0300

    muokkaus 1

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("test", "drew", "6986986");
+console.log("albolski");
+console.info("doggy")
+
+let x = 9;
+y = 110;
+const k = 32;
+let p;
+console.log(x , y , k , p);
+
+let str = "haukka"
+let f = 23.3
+let g = 12;
+let = 875358358585.0;
+let bool = true;
+let bool2 = false;
+let e;
+console.log("12323", e);
+let s = null;
+let u = undefined;
+
+hop = {y: 8,
+    x: 43
+};
+let rs = hop;
